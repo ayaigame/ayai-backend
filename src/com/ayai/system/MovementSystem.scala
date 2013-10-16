a337e60fcd773d6b6a37961c32a6231617bb0025
Jarrad Battaglia <jmbbat@gmail.com>
2013-10-16 17:20:16 -0400

    added some scala test files to build off of, and to show how to do some common things in scala for the components and systems. Also added jar file

---
+++ src/com/ayai/system/MovementSystem.scala
@@ -0,0 +1,25 @@
+package com.ayai.system
+
+import com.artemis.systems.EntityProcessingSystem
+import com.artemis.Aspect
+import com.ayai.Position
+import com.artemis.ComponentMapper
+import java.util.Map
+import com.artemis.Entity
+
+class MovementSystem(a: Aspect = Aspect.getAspectFor(classOf[Position]) ) extends EntityProcessingSystem(a) {    
+
+    private var positionMapper: ComponentMapper[Position] = _
+    
+    
+    override def initialize() {
+      println("initialized")
+      positionMapper = world.getMapper(classOf[Position])
+    }
+  
+    override def process(e: Entity) {
+        var p: Position = positionMapper.get(e)
+        p.setXPosition(0)
+        p.setYPosition(0)
+    }
+}
