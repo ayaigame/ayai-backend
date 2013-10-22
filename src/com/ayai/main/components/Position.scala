7e294caedff7e7d76d342ee3f52d21155fff98ef
Robert Mruczek <rtmruczek@gmail.com>
2013-10-22 14:38:01 -0400

    Organized eclipse project structure

---
+++ src/com/ayai/main/components/Position.scala
@@ -0,0 +1,29 @@
+package com.ayai.main.components
+
+import com.artemis.Component
+
+class Position extends Component{
+  private var x: Int = _
+  private var y : Int = _
+
+  def Position(x: Int, y: Int) {
+    this.x = x
+    this.y = y
+  }
+  
+  def getXPosition(): Int = {
+    return x
+  }
+  
+  def getYPosition(): Int = {
+    return y
+  }
+  
+  def setYPosition(y: Int) {
+    this.y = y
+  }
+  
+  def setXPosition(x: Int) {
+    this.x = x
+  }
+}
