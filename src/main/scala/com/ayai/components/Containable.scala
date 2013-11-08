ff253b9261027ca6a1b1bee2a64ecb77c9ac294a
maddchickenz <jmbbat@gmail.com>
2013-11-07 19:50:38 -0500

    added a gamemap class Created a item class created create item in entityfactory have automatic moving in movement system (need to add controls from networking) print out map when running (every 10 seconds) put map in networking instead fo Data.map

---
+++ src/main/scala/com/ayai/components/Containable.scala
@@ -0,0 +1,7 @@
+package com.ayai.main.components
+
+import com.artemis.Component
+
+class Containable(var id : Int, var name : String) extends Component {
+	
+}
---
+++ src/main/scala/com/ayai/components/Inventory.scala
@@ -0,0 +1,7 @@
+package com.ayai.components
+
+import com.artemis.Component
+import com.ayai.main.components.Containable 
+class Inventory (var inventory : Array[Containable]) extends Component {
+
+}
