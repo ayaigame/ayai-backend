6af6cac1dd12ac21857f6e64a03a4e66cc99ff72
Kurt <kurt.wheeler91@gmail.com>
2013-11-23 17:40:58 -0500

    Removes old class files.

--- src/main/scala/ayai/networking/NetworkMessage.scala
+++ src/main/scala/ayai/networking/NetworkMessage.scala
@@ -1,8 +1,4 @@
 package ayai.networking
 
-// abstract class NetworkMessage(connectionId: Int) {
-//   def getConnectionId: Int = connectionId
-// }
-
 case class NetworkMessage(message: String)
 
--- src/main/scala/ayai/networking/PlayerRequest.scala
+++
@@ -1,9 +0,0 @@
-// package ayai.networking
-
-// /** Akka Imports **/
-// import akka.actor.Actor
-
-// class PlayerRequest(connectionId: Int, playerId: Int) extends NetworkMessage(connectionId) {
-//   def getPlayerId: Int = playerId
-// }
-
