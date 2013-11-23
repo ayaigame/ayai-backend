40f49205b31d068e0207399eda47890997ebe50e
Jarrad <jmbbat@gmail.com>
2013-11-23 16:17:59 -0500

    changed from m floats to ints

--- src/main/scala/ayai/components/Velocity.scala
+++ src/main/scala/ayai/components/Velocity.scala
@@ -2,5 +2,5 @@ package ayai.components
 
 import com.artemis.Component
 
-class Velocity (var x : Float, var y : Float) extends Component {
+class Velocity (var x : Int, var y : Int) extends Component {
 }
