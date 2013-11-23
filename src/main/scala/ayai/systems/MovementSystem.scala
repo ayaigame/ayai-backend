5e06f0d82dba111d31e86d4340c5f7ba6f8034c8
Jarrad <jmbbat@gmail.com>
2013-11-23 16:52:51 -0500

    added actions and movement added example movement app added effects and added to containable

--- src/main/scala/ayai/systems/MovementSystem.scala
+++ src/main/scala/ayai/systems/MovementSystem.scala
@@ -17,14 +17,6 @@ class MovementSystem(map : GameMap, a: Aspect = Aspect.getAspectFor(classOf[Posi
   var velocityMapper: ComponentMapper[Velocity] = _
   
       override def process(e: Entity) {
-        var p: Position = positionMapper.get(e)
-        var velocity : Velocity = velocityMapper.get(e)
-        var test : Int = world.delta.intValue
-        //move position over one
-        if(!(p.x+1 < 0 ||  p.x+1 > map.width || p.y+1 < 0 || p.y+1 > map.height)) {
-          map.moveEntity(e.getId(),p.x,p.y)
-          p.x +=1
-          p.y +=1
         }
 
                 //p.x += velocity.speed*world.delta
