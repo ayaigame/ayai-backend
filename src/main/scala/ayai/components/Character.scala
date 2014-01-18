897ecc2a3ea16de48a058166cc5060004338cd05
jarrad-desktop <jmbbat@gmail.com>
2014-01-17 20:57:02 -0800

    fixed the fix

--- src/main/scala/ayai/components/Character.scala
+++ src/main/scala/ayai/components/Character.scala
@@ -4,6 +4,6 @@ import com.artemis.Component
 
 class Character(val characterId: String) extends Component{
 	  override def toString: String = {
-	  	return "\"" + characterId "\""
+	  	return "\"" + characterId + "\""
   }
 }
