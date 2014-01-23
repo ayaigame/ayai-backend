6e01b87c3b5a7cb3ddcb77d288ff9a4ae7e16db7
Joshua Henry <henry41043@gmail.com>
2014-01-23 18:42:47 -0500

    need to look up scala inheritance

--- src/main/scala/ayai/components/AICharacter.scala
+++ src/main/scala/ayai/components/AICharacter.scala
@@ -3,6 +3,6 @@ package ayai.components
 import com.artemis.Component
 import com.artemis.Entity
 
-class AICharacter(val characterId: String) extends Character {
+//class AICharacter(val characterId: String) extends Character {
 	
-}
+//}
