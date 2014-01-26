da9c4d3b1e5d021ef115afaeae3588d7371026e9
Joshua Henry <henry41043@gmail.com>
2014-01-26 14:11:08 -0500

    updating a*

--- src/main/scala/ayai/components/character_ai/AISystem.scala
+++ src/main/scala/ayai/components/character_ai/AISystem.scala
@@ -7,7 +7,35 @@ class AISystem(range : Int) {
 	val sightRange = range
 
 	def Astar(start : Position, target : Position, map : TileMap) = {
-		
+		val closed = List[Position]()
+		val open = List[Position]()
+		val cameFrom = List[Position]()
+		val cost = 0
+
+		open.+(start)
+		while(!open.isEmpty){
+			var current = open[0]
+			if(current == target){
+				cameFrom.+(target)
+				return cameFrom
+			}
+			// remove current from open set
+			open = open.tail
+			// add current to closed set
+			closed.+(current)
+
+			var neighbors = findMoves(current)
+			for( neighbor <- neighbors) {
+				if(closed.contains(neighbor)){
+					continue
+				}
+				
+			}
+		}
+	}
+
+	def findMoves(start : Position) = List[Position] {
+		// find all open tiles around start position
 	}
 
 	def findTarget(start : Position) = List[Entity] {
