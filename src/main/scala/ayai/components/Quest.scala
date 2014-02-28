254dedeb6f3b7e12f051afeb92a95b0656821ef1
jarrad-desktop <jmbbat@gmail.com>
2014-02-27 22:19:58 -0500

    made objectives a list

--- src/main/scala/ayai/components/Quest.scala
+++ src/main/scala/ayai/components/Quest.scala
@@ -5,14 +5,16 @@ import ayai.quests.{KillObjective}
 import net.liftweb.json.JsonDSL._
 import net.liftweb.json._
 
+import scala.collection.mutable.ArrayBuffer
+
 class Quest(var id: Int, var title: String, var description: String,
-			var recommendLevel: Int, var objective: KillObjective) extends Component {
+			var recommendLevel: Int, var objectives: List[KillObjective]) extends Component {
 
 	def asJson(): JObject = {
 		("id" -> id) ~
 		("title" -> title) ~
 		("description" -> description) ~
 		("recommendLevel" -> recommendLevel) ~
-		("objective" -> objective.asJson)
+		("objectives" -> objectives.map{obj => obj.asJson})
 	}
 }
