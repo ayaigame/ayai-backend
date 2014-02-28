5e82718df78cf14b03d8fb1c13cc1261c99bc7dc
Joshua Henry <henry41043@gmail.com>
2014-02-28 16:06:41 -0500

    merged some A* functionality

--- src/main/scala/ayai/systems/AISystem.scala
+++ src/main/scala/ayai/systems/AISystem.scala
@@ -57,7 +57,58 @@ class AISystem() extends System {
     }
 
   }
+  /*
+  def Astar(start : Position, target : Position) = List[Position] {
+    val closed = List[Position]()
+    val open = List[Position]()
+    val cameFrom = List[Position]()
 
+    open.+(start)
+    while(!open.isEmpty){
+      val current = open[0]
+      cameFrom.+(current)
+      if(current == target){
+        cameFrom.+(target)
+        return cameFrom
+      }
+      // remove current from open set
+      open = open.tail
+      // add current to closed set
+      closed.+(current)
+
+      val neighbors = findMoves(current)
+      val score = getScore(current, target)
+      for( neighbor <- neighbors) {
+        val repeat = false 
+        val alreadyQueued = false
+        val tempScore = getScore(neighbor, target)
+        if(closed.contains(neighbor)){
+          repeat = true 
+        }
+        if(repeat && tempScore >= score){
+          // do nothing
+        }
+        else{
+          if(open.contains(neighbor)){
+            alreadyQueued = true 
+          }
+          if(!alreadyQueued || tempScore < score){
+            score = tempScore
+            if(!alreadyQueued){
+              open.+(neighbor)
+              open = sortByScore(open, target)
+            }
+          }
+        }
+      }
+    }
+    // if it makes it here, there is no path to target
+  }
+
+  def findMoves(start : Position) = List[Position] {
+    // Checks all adjacent positions for walkability and returns list of possible new positions
+  }
+  */
   
   override def process(delta: Int) {
     val entities = world.getEntitiesByComponents(classOf[AI], classOf[Position])
