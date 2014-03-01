7c3968330eb859e1c0c56585f1a6b1812c34dcd8
Joshua Henry <henry41043@gmail.com>
2014-03-01 17:40:35 -0500

    making AI similar to character

--- src/main/scala/ayai/components/AI.scala
+++ src/main/scala/ayai/components/AI.scala
@@ -3,4 +3,21 @@ package ayai.components
 /** Crane Imports **/
 import crane.Component
 
-class AI() extends Component
+/** External Imports **/
+import net.liftweb.json.Serialization.{read, write}
+import net.liftweb.json.JsonDSL._
+import net.liftweb.json._
+
+class AI(id: String, name: String, level: Int) extends Component {
+	implicit val formats = Serialization.formats(NoTypeHints)
+
+ 	implicit def asJson() : JObject = {
+    	("id" -> id) ~
+    	("name" -> name) ~
+    	("level" -> level)
+  	}
+
+  	override def toString: String = {
+    	write(this)
+  	}
+}
