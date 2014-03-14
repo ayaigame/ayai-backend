78733130ccac7d57f8d5256461ac9240ba772588
Kurt <kurt.wheeler91@gmail.com>
2014-03-13 20:44:32 -0400

    Adds item saving and deletion to InventoryTable.

--- src/main/scala/ayai/persistence/InventoryTable.scala
+++ src/main/scala/ayai/persistence/InventoryTable.scala
@@ -22,6 +22,83 @@ import net.liftweb.json._
 import net.liftweb.json.Serialization.{read, write}
 
 object InventoryTable {
+
+  //MUST BE CALLED FROM WITHIN TRANSACTION
+  //This is so it can be done multiple times within a single transaction.
+  def upsertItem(inventoryRow: InventoryRow) {
+    val itemQuery =
+      from(AyaiDB.inventory)(row =>
+        where(row.playerId === inventoryRow.playerId
+          and row.itemId === inventoryRow.itemId)
+        select(row)
+      )
+
+    if(itemQuery.size == 0) {
+      AyaiDB.inventory.insert(inventoryRow)
+    }
+    else if (itemQuery.size == 1) {
+      AyaiDB.inventory.update(inventoryRow)
+    }
+    else {
+      println("Error: duplicate inventory row.")
+    }
+  }
+
+  //MUST BE CALLED FROM WITHIN TRANSACTION
+  //This is so it can be done multiple times within a single transaction.
+  //Deletes all copies of the item in the character's inventory
+  def deleteAllOfItem(itemEnity: Entity, characterEntity: Entity) {
+   (entity.getComponent(classOf[Item]),
+      entity.getComponent(classOf[Character])) match {
+      case (Some(item: Item), Some(character: Character)) =>
+        // val itemQuery =
+        //   from(AyaiDB.inventory)(row =>
+        //     where(row.playerId === character.id
+        //       and row.itemId === item.id)
+        //     select(row)
+        //   )
+
+        // if(itemQuery.size > 0) {
+          AyaiDB.inventory.deleteWhere(row =>
+            (row.playerId === character.id) and
+            (row.itemId === item.id)
+          )
+        // }
+  }
+
+  //MUST BE CALLED FROM WITHIN TRANSACTION
+  //This is so it can be done multiple times within a single transaction.
+  def deleteItem(itemEnity: Entity, characterEntity: Entity) {
+   (entity.getComponent(classOf[Inventory]),
+      entity.getComponent(classOf[Character])) match {
+      case (Some(item: Item), Some(character: Character)) =>
+        val itemQuery =
+          from(AyaiDB.inventory)(row =>
+            where(row.playerId === character.id
+              and row.itemId === item.id)
+            select(row)
+          )
+
+        if (itemQuery.size == 1) {
+          newRow = itemQuery.single
+          newRow.quantity = newRow.quantity - 1
+          if(newRow.quantity > 0)
+            AyaiDB.inventory.update(newRow)
+          else {
+            AyaiDB.inventory.deleteWhere(row =>
+              (row.playerId === character.id) and
+              (row.itemId === item.id)
+            )
+          }
+        }
+        else {
+          println("Error: duplicate inventory row.")
+        }
+      case _ =>
+        println("Error in InventoryTable")
+    }
+  }
+
   def saveInventory(entity: Entity) {
     (entity.getComponent(classOf[Inventory]),
       entity.getComponent(classOf[Character])) match {
@@ -38,7 +115,7 @@ object InventoryTable {
             println(s"Inserting $inventoryRows")
 
             transaction {
-              inventoryRows foreach ((inventoryRow: InventoryRow) => AyaiDB.inventory.insertOrUpdate(inventoryRow))
+              inventoryRows map upsertItem
             }
           case _ =>
             println(s"Can't find account for $character.name")
